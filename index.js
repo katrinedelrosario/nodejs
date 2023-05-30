2d7c542473c103978e28da6fdf6e61945937b4d4
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-05-30 13:02:27 +0200

    started setting up pages

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ app.get("/", (req, res) => {
     res.send('hii world');
 })
 
+app.get("/about", (req, res) => {
+    res.send('about');
+})
+
+app.get("/contact", (req, res) => {
+    res.send('contact');
+})
+
+app.get("/products", (req, res) => {
+    res.send('products');
+})
+
+app.get("/", (req, res) => {
+    res.send('hii world');
+})
+
+app.use((req, res, next) => {
+    res.status(404).send('site not found :(')
+})
