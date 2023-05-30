2e091a27b633cbe10a547bc9addd0245c1999f66
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-05-30 12:18:41 +0200

    -

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import http from 'http';
 
 http.createServer((req, res) => {
-    console.log('server is running on http://localhost:4000')
+    console.log('node server is running on http://localhost:4000') 
     res.writeHead(400, {'Content-Type': 'text/html'}),
     res.write('hiii world')
     res.end()
-}).listen(4000)
+}).listen(4000) 
